01cecdc966c8019794879784ba35b5e510838964
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-01 02:32:56 -0500

    Update read.py

--- Actividad 1er Semana 27-28-Abril/Collections-Tuple/read.py
+++ Actividad 1er Semana 27-28-Abril/Collections-Tuple/read.py
@@ -1,14 +1,13 @@
 
 import pickle
 
-#Para la lectura del binario ahora se utiliza rb, se podria decir que el archivo
-#binario se obtiene y se carga para despues imprimirlo entonces de esta forma llamo,
-#a mi archivo binario y puedo cargar su contenido
+#Utilizo 'rb' señalando que es para modo lectura,
+#y los datos del .bin los transcribo al nuevo elemento,
+#WebDev se cargan los datos para utilizarlos despues.
 with open('Tuples.bin','rb') as fh:
     WebDev=pickle.load(fh)
     
-    #para la impresion de la coleccion se puede utilizar for o map se puede conseguir,
-    #que la coleccion salga sin corchete o comillas y esto se usa en general para el
-    #manejo de los datos en las colecciones aunque se puede usar para visual en este caso.
+    #impresion de los datos utilizando map() para mejor,
+    #lectura de los mismos.
     print('\n'.join(map(str, WebDev)))
     print(type(WebDev))
