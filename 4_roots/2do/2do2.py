87d355acb977be40886241f7125c1c1ba14d473f
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-12 10:27:28 -0500

    Semana 2

--- 4_roots/2do/2do2.py
+++ 4_roots/2do/2do2.py
@@ -14,3 +14,22 @@
 # it will produce 100 points
 # [1.0, 1.1, ... , 9.9, 10]
 #
+# -*- coding: utf-8 -*-
+"""
+Created on  Wed May 10 10:11:15 2022
+@author: naran
+"""
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+secuencia = (x/10 for x in range(1, 100))
+for x in secuencia:
+    px = np.array([5, x])
+
+py = np.array([1, 30])
+plt.plot(px, py)
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
