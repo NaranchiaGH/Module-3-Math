fe8ab32e98f58435e1a446b18ac5fc1bbe3076a8
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-12 12:29:32 -0500

    Semana 2

--- 4_roots/2do/2do1.py
+++ 4_roots/2do/2do1.py
@@ -13,27 +13,28 @@ import matplotlib.pyplot as plt
 from math import exp
 
 
-def f(x):  # Función
-    return exp(2-x)-7*x
+def fn(x):
+    return exp(2-x)-7*(x)
 
 
-def f2(x):  # Derivada
-    return -exp(2-x)-7
+def f2(x):
+    return -exp(2-x)/-7
 
 
-x0 = 1.5
+x0 = 1.055
 
-x1 = x0-f(x0)/f2(x0)
-print('x1 = ', x1)
-
-x2 = x1-f(x1)/f2(x1)
+x1 = -exp(2-x0)/-7
+print("x1 = ", x1)
+x2 = -exp(2-x1)/-7
 print('x2 = ', x2)
+x3 = -exp(2-x2)/-7
+print("x3 = ", x3)
+x4 = -exp(2-x3)/-7
+print("x4 = ", x4)
+x5 = -exp(2-x4)/-7
+print("x5 = ", x5)
+print("La raiz de e^(2-x)-7x=0 es: ", x5)
 
-x3 = x2-f(x2)/f2(x2)
-print('x3 = ', x3, '\n')
-print('valor = ', f(x3))
-print('La raiz es: ', x3)
-
-xplt = [-2, -1, 0, 1, 2]
-yplt = [-61.59, -27.08, -14.38, -9.71, 8]
-print(plt.scatter(xplt, yplt))
+#xplt = [-2, -1, 0, 1, 2]
+#yplt = [-61.59, -27.08, -14.38, -9.71, 8]
+#print(plt.scatter(xplt, yplt))
