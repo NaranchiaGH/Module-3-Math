5d1153c5a62faeb5969ec9cca7fbc6da4c2ef401
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-12 11:44:11 -0500

    Semana 1

--- 2_fundamentals/2do/stats.py
+++ 2_fundamentals/2do/stats.py
@@ -2,17 +2,24 @@
 # select and write 
 # one of the following stat functions
 #
+#def sum(lst): pass
+#def avg(l): pass
+#def min(l): pass
+#def max(l): pass
+#def range(l): pass
+#def std(l): pass
+#def mode(l): pass
+@author: "naran"
+import random
+import statistics
 
-def sum(lst): pass
 
-def avg(l): pass
+def mode(array):
+    List = [random.randint(1, 10) for x in range(50)]
+    print(List)
+    moda = statistics.mode(List)
+    pass
+    return moda
 
-def min(l): pass
 
-def max(l): pass
-
-def range(l): pass
-
-def std(l): pass
-
-def mode(l): pass
+print("La moda de este conjunto de numeros aleatorios es: ", mode(10))
