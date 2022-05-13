d59d79007f65ce52e8ba5e7f0cf8ceb1a2043172
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-12 20:10:01 -0500

    Semana 2
    Actividades para el 4 y 5 de Mayo
    Temas:
    Interpolación y Solución de ecuaciones no lineales

--- 3_interpolation/2do/2do.py
+++
@@ -1,8 +0,0 @@
-#
-# refer to PPT file
-# for exercise
-# Lista de activides
-#
-#2do_1 |Metodo Lineal| Calcular x cuando y es 3.7 || Dados: (-4, -2) y (1, 5) 
-#2do_2 |Metodo Newton| Estimar y, cuando x = 7.7 || Dados: (-2,4), (-1,-2), (3,5) y (4.3,11)
-#2do_3 |Metodo Lagrange| Estimar el polinomio interpolante donde || (-1,3), (0,-7), (4,7) y (5,11)
