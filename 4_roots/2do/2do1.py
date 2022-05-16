1c6a925d2619957a4e9a469f9fbdf36d1dca51a3
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-15 21:19:13 -0500

    Semana 2
    Actividades para el 4 y 5 de mayo
    Temas:
    Interpolación y Solución de ecuaciones no lineales

--- 4_roots/2do/2do1.py
+++ 4_roots/2do/2do1.py
@@ -12,27 +12,23 @@ import matplotlib.pyplot as plt
 from math import exp
 
 
-def fn(x):
-    return exp(2-x)-7*(x)
+def f(x):
+    return exp(2-x)-7*x
 
 
 def f2(x):
-    return -exp(2-x)/-7
+    return -exp(2-x)-7
 
 
-x0 = 1.055
+x0 = 1.5
+x1 = x0-f(x0)/f2(x0)
+print('x1 =', x1)
+x2 = x1-f(x1)/f2(x1)
+print('x2 =', x2)
+x3 = x2-f(x2)/f2(x2)
+print('x3= ', x3)
 
-x1 = -exp(2-x0)/-7
-print("x1 = ", x1)
-x2 = -exp(2-x1)/-7
-print('x2 = ', x2)
-x3 = -exp(2-x2)/-7
-print("x3 = ", x3)
-x4 = -exp(2-x3)/-7
-print("x4 = ", x4)
-x5 = -exp(2-x4)/-7
-print("x5 = ", x5)
-print("La raiz de e^(2-x)-7x=0 es: ", x5)
+print("La raiz de e^(2-x)-7x=0 es: ", x3)
 
 #xplt = [-2, -1, 0, 1, 2]
 #yplt = [-61.59, -27.08, -14.38, -9.71, 8]
