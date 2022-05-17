b6f1e0b68131c78bdaad1ca9d37e06bce527fa75
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-16 23:55:08 -0500

    Semana 3
    Actividades para el 11 y 12 de mayo
    Temas:
    Derivación e integración numérica

--- 6_Integration/2do/2do_2.py
+++ 6_Integration/2do/2do_2.py
@@ -2,6 +2,7 @@
 # dado (7^(-x))+3 cuando a = -1 y b = 2
 # minimo 2 metodos a elegir
 
+
 def f(x):
     return (7**(-x))+3
 
@@ -9,14 +10,8 @@ def f(x):
 a = -1
 b = 2
 
-r1 = f(a)*(b-a)
-print("Regla del rectangulo =", r1)
+metodo_1 = f((a+b)/2)*(b-a)
+print('Regla del punto medio =', metodo_1)
 
-r2 = f((a+b)/2)*(b-a)
-print('Regla del punto medio =', r2)
-
-r3 = ((b-a)/2)*(f(a)+f(b))
-print('Regla del trapecio =', r3)
-
-r4 = ((b-a)/6)*(f(a)+(4*f((a+b)/2)+f(b)))
-print('Regla de Simpson =', r4)
+metodo_2 = ((b-a)/6)*(f(a)+(4*f((a+b)/2)+f(b)))
+print('Regla de Simpson =', metodo_2)
