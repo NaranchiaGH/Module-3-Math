62055d2bfac97efba2c868697d03ffba7845db5f
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-17 01:37:24 -0500

    Semana 2 / Correcciones
    Actividades para el 4 y 5 de mayo
    Temas:
    Interpolación y Solución de ecuaciones no lineales

--- 4_roots/2do/2do2.py
+++ 4_roots/2do/2do2.py
@@ -1,22 +1,31 @@
 #
 # generate a function to produce points 
+# to be used as x-axis
+# 
+# INPUT
+# -> a initial point
+# -> b final point
+# -> c increment
 #
-# -*- coding: utf-8 -*-
-"""
-Created on  Wed May 10 10:11:15 2022
-@author: naran
-"""
-import sys
-import matplotlib.pyplot as plt
+# OUTPUT
+# <- list of points
+#
+# for instance for this range [1,10,.1]
+# it will produce 100 points
+# [1.0, 1.1, ... , 9.9, 10]
 import numpy as np
+import matplotlib.pyplot as plt
 
-secuencia = (x/10 for x in range(1, 100))
-for x in secuencia:
-    px = np.array([5, x])
+a = float(input("Ingresa el punto inicial: "))
+b = float(input("Ingresa el punto final: "))
+c = float(input("Ingresa el incremento: "))
+x = np.arange(a, b, c)
+y = [Xn**1 for Xn in x]
 
-py = np.array([1, 30])
-plt.plot(px, py)
+plt.title("produce points")
+plt.scatter(x, y, edgecolors=('red'))
 plt.show()
 
-plt.savefig(sys.stdout.buffer)
-sys.stdout.flush()
+# rango x - axis
+# plt.axis(xmin=i_point, xmax=f_point)
+
