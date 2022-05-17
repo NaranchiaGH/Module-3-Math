48842c7b513ec566ebea849e92dc1928657f3da2
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-16 23:46:32 -0500

    Semana 3
    Actividades para el 11 y 12 de mayo
    Temas:
    Derivación e integración numérica

--- 6_Integration/2do/2do_1.py
+++ 6_Integration/2do/2do_1.py
@@ -12,15 +12,11 @@ def f(x):
 
 a = 0
 b = 1.9724
+metodo_1 = ((b-a)/6)*(f(a)+(4*f((a+b)/2)+f(b)))
+print('Regla de Simpson =', metodo_1)
 
-metod_1 = f(a)*(b-a)
-print('Regla del rectangulo =', metod_1)
+metodo_2 = f((a+b)/2)*(b-a)
+print('Regla del punto medio =', metodo_2)
 
-metod_2 = f((a+b)/2)*(b-a)
-print('Regla del punto medio =', metod_2)
-
-metod_3 = ((b-a)/2)*(f(a)+f(b))
-print('Regla del trapecio =', metod_3)
-
-metod_4 = ((b-a)/6)*(f(a)+(4*f((a+b)/2)+f(b)))
-print('Regla de Simpson =', metod_4)
+metodo_3 = ((b-a)/2)*(f(a)+f(b))
+print('Regla del trapecio =', metodo_3)
