ff461520e6edccde660229a9eb3460247966b39f
NaranchiaGH <85897141+NaranchiaGH@users.noreply.github.com>
2022-05-21 16:55:16 -0500

    Delete Actividades 2da Semana 4-5-May directory

--- Actividades 2da Semana 4-5-May/Interpolaciones/Interpolacion-Lineal/Interp_Lineal_X.py
+++
@@ -1,7 +0,0 @@
-# Interpolation Lineal
-# (-4, -2) y (1, 5) estimar valor para x cuando y = 3.7
-x0 = -4; x1 = 1
-y = 3.7; y0 = -2; y1 = 5
-
-x = ((x1 - x0) / (y1 - y0) * (y - y0) + x0)
-print("El valor para x cuando y = 3.7 es: ", x)
